d346ca7e04f9740a90cd0c3efc0ab742f62fb02a
Dilaver Şerif <dilaver.serif@outlook.com>
2020-12-30 20:19:47 +0300

    v0.1

---
+++ Assets/Scripts/Control.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Control : MonoBehaviour, IDragHandler, IPointerDownHandler, IPointerUpHandler
+{
+    private Vector2 ilkVector, sonVector;
+    private float aci;
+    [SerializeField]
+    public GameObject asd;
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        sonVector = eventData.position;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        ilkVector = eventData.position;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        aci = Vector2.Angle(ilkVector,sonVector);
+        //asd.transform.rotation = Quaternion.Euler(0, 0, aci);
+        //Debug.Log(sonVector + " " + ilkVector + "ACI " + aci);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Vector2 ilk, son;
+    public GameObject sd;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            ilk = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            son = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            float mesafe = son.y - ilk.y;
+            Vector2 fark = ilk - son;
+            float Angle = Mathf.Atan2(fark.y, fark.x);
+            float AngleInDegrees = Angle * Mathf.Rad2Deg;
+            float sonuc = Vector3.Angle(ilk, son);
+            Debug.Log(AngleInDegrees + " " + ilk);
+            sd.transform.rotation = Quaternion.Euler(0, 0, AngleInDegrees - 90);
+            sd.transform.localScale = new Vector3(1,mesafe,1);
+        }
+    }
+
+}
